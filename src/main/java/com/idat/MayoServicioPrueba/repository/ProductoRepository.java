580c7756b67e2ea55b2998466f0f932ef585c095
SuarezLuigui <105759405+SuarezLuigui@users.noreply.github.com>
2022-05-20 20:09:52 -0500

    Inicio de Proyecto

---
+++ src/main/java/com/idat/MayoServicioPrueba/repository/ProductoRepository.java
@@ -0,0 +1,14 @@
+package com.idat.MayoServicioPrueba.repository;
+
+import java.util.List;
+
+import com.idat.MayoServicioPrueba.model.Productos;
+
+public interface ProductoRepository {
+	
+	void guardadProducto(Productos producto);
+	void actualizarProducto(Productos producto);
+	void eliminarProducto(Integer id);
+	List<Productos> listarProductos();
+	Productos obtenerProductoId(Integer id);
+}
---
+++ src/test/java/com/idat/microservicioidatAulaO/MayoServicioPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.microservicioidatAulaO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MayoServicioPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
